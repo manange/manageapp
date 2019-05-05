ea368de67b78b959cba88582508ec2a94527d463
mfaizudd <mfaizudd@gmail.com>
2019-05-05 11:43:35 +0700

    Change BarView to BarChart

--- app/src/main/java/com/example/manageapp/StatsFragment.java
+++ app/src/main/java/com/example/manageapp/StatsFragment.java
@@ -3,6 +3,7 @@ package com.example.manageapp;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Debug;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,14 +12,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
-import im.dacer.androidcharts.BarView;
+import java.util.TreeMap;
 
 
 public class StatsFragment extends Fragment {
@@ -28,13 +39,14 @@ public class StatsFragment extends Fragment {
     final String KEY_DATE = "date";
 
     private ArrayList<HashMap<String, String>> stats = new ArrayList<>();
-    BarView barView;
+
+    BarChart barChart;
     TaskDBHelper mydb;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        barView = view.findViewById(R.id.chart);
+        barChart = view.findViewById(R.id.chart);
         mydb = new TaskDBHelper(this.getContext());
         new LoadTask().execute();
     }
@@ -61,36 +73,82 @@ public class StatsFragment extends Fragment {
 
             return xml;
         }
-
         @Override
         protected void onPostExecute(String xml) {
-            HashMap<String, Integer> entries = new HashMap<>();
-            ArrayList<String> bottomTexts = new ArrayList<>();
-            ArrayList<Integer> dataList = new ArrayList<>();
+//            HashMap<String, Integer> entries = new HashMap<>();
+//            ArrayList<String> bottomTexts = new ArrayList<>();
+//            ArrayList<Integer> dataList = new ArrayList<>();
+//            for(int i = 0; i < stats.size(); i++) {
+//                String date = stats.get(i).get(KEY_DATE);
+//                if(!entries.containsKey(date)) {
+//                    entries.put(date, 1);
+//                }
+//                else {
+//                    int total = entries.get(date);
+//                    entries.put(date, total+1);
+//                }
+//            }
+//            Iterator it =  entries.entrySet().iterator();
+//            int max = 0;
+//            while(it.hasNext()) {
+//                Map.Entry pair = (Map.Entry)it.next();
+//                bottomTexts.add(pair.getKey().toString());
+//                int total = Integer.parseInt(pair.getValue().toString());
+//                if(max < total) {
+//                    max = total;
+//                }
+//                dataList.add(total);
+//                it.remove();
+//            }
+//            barChart.setBottomTextList(bottomTexts);
+//            barChart.setDataList(dataList, max);
+
+            Collections.sort(stats, new Comparator<HashMap<String, String>>() {
+                DateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+                @Override
+                public int compare(HashMap<String, String> o1, HashMap<String, String> o2) {
+                    try {
+                        return format.parse(o1.get(KEY_DATE)).compareTo(format.parse(o2.get(KEY_DATE)));
+                    } catch (ParseException e) {
+                        throw new IllegalArgumentException(e);
+                    }
+                }
+            });
+
+            final List<BarEntry> entries = new ArrayList<>();
+            final Map<String, Integer> entriesMap = new TreeMap<>();
+            final List<String> entriesLabel = new ArrayList<>();
             for(int i = 0; i < stats.size(); i++) {
                 String date = stats.get(i).get(KEY_DATE);
-                if(!entries.containsKey(date)) {
-                    entries.put(date, 1);
-                }
-                else {
-                    int total = entries.get(date);
-                    entries.put(date, total+1);
+                if(!entriesMap.containsKey(date)) {
+                    entriesMap.put(date, 1);
+                } else {
+                    int total = entriesMap.get(date);
+                    entriesMap.put(date, total+1);
                 }
             }
-            Iterator it =  entries.entrySet().iterator();
-            int max = 0;
+            Iterator it = entriesMap.entrySet().iterator();
+            int index = 0;
             while(it.hasNext()) {
                 Map.Entry pair = (Map.Entry)it.next();
-                bottomTexts.add(pair.getKey().toString());
-                int total = Integer.parseInt(pair.getValue().toString());
-                if(max < total) {
-                    max = total;
-                }
-                dataList.add(total);
-                it.remove();
+                int total = (int)pair.getValue();
+                entries.add(new BarEntry(index, total));
+                entriesLabel.add((String)pair.getKey());
+                index++;
             }
-            barView.setBottomTextList(bottomTexts);
-            barView.setDataList(dataList, max);
+
+            BarDataSet set = new BarDataSet(entries, "Tasks");
+            BarData data = new BarData(set);
+            barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter() {
+                @Override
+                public String getFormattedValue(float value) {
+                    int index = (int)value;
+                    return entriesLabel.get(index);
+                }
+            });
+            barChart.getXAxis().setLabelRotationAngle(-45);
+            barChart.setData(data);
+            barChart.invalidate();
         }
     }
 
